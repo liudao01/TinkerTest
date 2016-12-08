ac30414be42f03aad8b898a589e2561ba20e3f55
liuml <liudao1@qq.com>
2016-12-09 00:22:53 +0800

    first

---
+++ app/src/main/java/test2/www/tinkertest/MainActivity.java
@@ -0,0 +1,26 @@
+package test2.www.tinkertest;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            }
+        });
+    }
+}
