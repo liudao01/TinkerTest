ffa58967165aaf17226866fcb4fae11d187d62de
LiuMaoLinGit <951442843@qq.com>
2017-01-07 11:54:44 +0800

    test

--- app/src/main/java/test2/www/tinkertest/MainActivity.java
+++ app/src/main/java/test2/www/tinkertest/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
